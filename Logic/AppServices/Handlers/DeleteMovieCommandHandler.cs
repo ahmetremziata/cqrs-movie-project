24ba2e9bf7bb5e1f9361953802d033660af9eb18
ahmet.ata <Bursa9753*>
2021-05-18 09:24:20 +0300

    Feat: Change logic in delete movie business

--- Logic/AppServices/Handlers/DeleteMovieCommandHandler.cs
+++ Logic/AppServices/Handlers/DeleteMovieCommandHandler.cs
@@ -1,9 +1,10 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using CSharpFunctionalExtensions;
 using Logic.AppServices.Commands;
 using Logic.Data.DataContexts;
 using Logic.Data.Entities;
-using Logic.Data.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace Logic.AppServices.Handlers
@@ -24,8 +25,33 @@ namespace Logic.AppServices.Handlers
             {
                 return Result.Failure($"No movie found for Id {command.Id}");
             }
-
             _dataContext.Movies.Remove(movie);
+
+            List<MoviePerson> moviePersons =
+                await _dataContext.MoviePersons.Where(item => item.MovieId == command.Id).ToListAsync();
+
+            foreach (var moviePerson in moviePersons)
+            {
+                _dataContext.MoviePersons.Remove(moviePerson);
+            }
+            
+            List<MovieCountry> movieCountries =
+                await _dataContext.MovieCountries.Where(item => item.MovieId == command.Id).ToListAsync();
+
+            foreach (var movieCountry in movieCountries)
+            {
+                _dataContext.MovieCountries.Remove(movieCountry);
+            }
+            
+            List<MovieType> movieTypes =
+                await _dataContext.MovieTypes.Where(item => item.MovieId == command.Id).ToListAsync();
+
+            foreach (var movieType in movieTypes)
+            {
+                _dataContext.MovieTypes.Remove(movieType);
+            }
+            
+            await _dataContext.SaveChangesAsync();
             return Result.Success();
         }
     }
