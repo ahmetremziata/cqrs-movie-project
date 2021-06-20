e700f6fa34e09c9ddccb717622df4b067f6eff62
John Doe <john@doe.org>
2021-06-20 12:19:06 +0300

    Add movie deactive endpoint for sending event

--- Logic/AppServices/Commands/Handlers/DeactivateMovieCommandHandler.cs
+++ Logic/AppServices/Commands/Handlers/DeactivateMovieCommandHandler.cs
@@ -1,18 +1,29 @@
 using System.Threading.Tasks;
 using CSharpFunctionalExtensions;
+using Logic.Business.Service.Kafka.Interfaces;
 using Logic.Data.DataContexts;
 using Logic.Data.Entities;
+using Logic.Events;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace Logic.AppServices.Commands.Handlers
 {
     public sealed class DeactivateMovieCommandHandler : ICommandHandler<DeactivateMovieCommand>
     {
         private readonly MovieDataContext _dataContext;
-
-        public DeactivateMovieCommandHandler(MovieDataContext dataContext)
+        private readonly IConfiguration _configuration;
+        private readonly IProducerService _producerService;
+        private readonly ILogger<DeactivateMovieCommandHandler> _logger;
+        
+        public DeactivateMovieCommandHandler(MovieDataContext dataContext, IConfiguration configuration, IProducerService producerService, ILogger<DeactivateMovieCommandHandler> logger)
         {
             _dataContext = dataContext;
+            _configuration = configuration;
+            _producerService = producerService;
+            _logger = logger;
         }
         
         public async Task<Result> Handle(DeactivateMovieCommand command)
@@ -27,11 +38,25 @@ namespace Logic.AppServices.Commands.Handlers
             {
                 return Result.Failure($"Movie already passive for movieId {command.MovieId}");
             }
+            
+            var topic = _configuration["MovieDeactivatedTopicName"];
+
 
             movie.IsActive = false;
             await _dataContext.SaveChangesAsync();
             
-            //TODO: Add fat event for activated and send event
+            MovieDeactivedEvent movieDeactivatedEvent = new MovieDeactivedEvent
+            {
+                MovieId = command.MovieId
+            };
+            
+            var isSendEvent = await _producerService.Produce(topic, movieDeactivatedEvent);
+
+            if (!isSendEvent)
+            {
+                _logger.LogError(
+                    $"{nameof(ActivateMovieCommandHandler)} {nameof(Handle)} not produce movie deactivated event - Event:{JsonConvert.SerializeObject(movieDeactivatedEvent)} - Topic: {topic}");
+            }
             return Result.Success();
         }
     }
---
+++ Logic/Events/MovieDeactivedEvent.cs
@@ -0,0 +1,7 @@
+namespace Logic.Events
+{
+    public class MovieDeactivedEvent
+    {
+        public int MovieId { get; set; }
+    }
+}
